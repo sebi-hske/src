c273da073868aa25d082325b0ab756c0173f18d2
sebi-hske <ratzeray@gmail.com>
2025-05-11 16:50:30 +0200

    update central control, add position input for testing

--- ro45_portalrobot_controller/ro45_portalrobot_controller/central_control.py
+++ ro45_portalrobot_controller/ro45_portalrobot_controller/central_control.py
@@ -3,7 +3,11 @@ from rclpy.node import Node
 
 from ro45_portalrobot_interfaces.msg import RobotCmd
 from ro45_portalrobot_interfaces.msg import RobotPos
+from std_msgs.msg import Float32
 from pd_regler import PDRegler
+
+P_VALUE = 10.0
+D_VALUE = 0.1
 
 class CentralControl(Node):
     def __init__(self):
@@ -14,6 +18,14 @@ class CentralControl(Node):
             self.pos_callback,
             10
         )
+
+        self.subscription = self.create_subscription(
+            Float32,
+            'position_command',
+            self.cmd_callback,
+            10
+            )
+
         self.publisher = self.create_publisher(RobotCmd, 'robot_command', 10)
         self.msg = RobotCmd()
         self.msg.accel_x = 0.0
@@ -21,7 +33,7 @@ class CentralControl(Node):
         self.msg.accel_z = 0.0
         self.msg.activate_gripper = False
 
-        self.pd_control = PDRegler(1.0, 0.1)        #initialize PD controller
+        self.pd_control = PDRegler(P_VALUE, D_VALUE)        #initialize PD controller
 
         self.timer_period = 0.1
         self.timer = self.create_timer(self.timer_period, self.timer_callback)
@@ -31,13 +43,20 @@ class CentralControl(Node):
         self.pos_y = msg.pos_y
         self.pos_z = msg.pos_z
 
-    def communicate(self):
-                                    
-        fault = self.pd_control.berechne(1.0, self.pos_x, self.timer_period)    #set desired and actual value
+    def cmd_callback(self, msg):
+        self.desired_pos_x = msg.data
+       
+
+    def call_pd_controller(self):
+        try:  
+            fault = self.pd_control.berechne(self.desired_pos_x, self.pos_x, self.timer_period)    #set desired and actual value
+        except AttributeError:
+            self.get_logger().warn("Desired position not set. Please set the desired position first.")
+            fault = 0.0
         return fault
 
     def timer_callback(self):
-        fault = self.communicate()
+        fault = self.call_pd_controller()
         self.msg.accel_x = fault
         self.get_logger().info("Publishing -> accel_x: {:.2f}".format(self.msg.accel_x))
         self.publisher.publish(self.msg)
---
+++ ro45_portalrobot_controller/ro45_portalrobot_controller/pos_input.py
@@ -0,0 +1,50 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import Float32
+
+
+
+class PositionInput(Node):
+    def __init__(self):
+        super().__init__('position_input')
+        self.timer_period = 0.1
+        self.timer = self.create_timer(self.timer_period, self.timer_callback)
+
+        self.publisher = self.create_publisher(Float32, 'position_command', 10)
+        self.msg = Float32()
+
+    def timer_callback(self):
+        self.get_position()
+        
+
+    def get_position(self):
+        try:
+            pos_cmd = input(f"Enter desired position: ")
+           
+            if pos_cmd.strip():
+                self.msg.data = float(pos_cmd)
+            
+
+        except ValueError:
+            self.get_logger().warn("Invalid input. Please enter numeric values.")
+
+        
+        self.publisher.publish(self.msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    publisher = PositionInput()
+
+    try:
+        rclpy.spin(publisher)
+    except KeyboardInterrupt:
+        pass
+
+    publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
