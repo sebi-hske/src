b4fb78953a3847e3204904ce9bcdd5a04ada2264
sebi-hske <ratzeray@gmail.com>
2025-05-27 10:01:47 +0200

    calibration and action system in place

--- ro45_portalrobot_controller/ro45_portalrobot_controller/central_control.py
+++ ro45_portalrobot_controller/ro45_portalrobot_controller/central_control.py
@@ -6,9 +6,13 @@ from ro45_portalrobot_interfaces.msg import RobotPos
 from std_msgs.msg import Float32
 from pd_regler import PDRegler
 from ro45_action_interfaces.action import MovetoPos
+from rclpy.action import ActionServer, CancelResponse, GoalResponse
+import threading
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
 
 P_VALUE_X = 0.1
-D_VALUE_X = 0.7
+D_VALUE_X = 0.9 # up from 0.7 default, possible delay in execute callback
 
 P_VALUE_Y = 0.08
 D_VALUE_Y = 0.7
@@ -27,12 +31,24 @@ class CentralControl(Node):
         )
         
         #only for testing
+        """
         self.subscription_poscmd = self.create_subscription(
             RobotPos,
             'user_input_position',
             self.cmd_callback,
             10
             )
+        """
+        self.goal_handle = None
+        self.action_server = ActionServer(
+            self,
+            MovetoPos,
+            'move_to_position',
+            execute_callback=self.execute_callback,
+            goal_callback=self.goal_callback,
+            handle_accepted_callback=self.handle_accepted_callback,
+            cancel_callback=self.cancel_callback
+        )
         
         self.publisher_cmd = self.create_publisher(RobotCmd, 'robot_command', 10)
         self.msg = RobotCmd()
@@ -40,23 +56,56 @@ class CentralControl(Node):
         self.msg.accel_y = 0.0      
         self.msg.accel_z = 0.0
         self.msg.activate_gripper = False
+
+        self.goal_lock = threading.Lock()
+        self.callback_group = ReentrantCallbackGroup()
         #initialize PD controller
         self.pd_control_x = PDRegler(P_VALUE_X, D_VALUE_X)   
         self.pd_control_y = PDRegler(P_VALUE_Y, D_VALUE_Y)
         self.pd_control_z = PDRegler(P_VALUE_Z, D_VALUE_Z)
         
         self.calibration()
+
+    def execute_callback(self, goal_handle):
+        self.get_logger().info("Executing goal...")
+        self.timer_period = 0.01
+        self.timer = self.create_timer(self.timer_period, self.timer_callback)
+        goal_handle.succeed()    
+        result = MovetoPos.Result()    
+        return result
+    
+    def goal_callback(self, goal_request):
+        self.get_logger().info("Received goal request to move to position: " + str(goal_request))
+        self.desired_pos_x = (goal_request.position_x - self.corr_val_x) * -1.0
+        self.desired_pos_y = (goal_request.position_y - self.corr_val_y) * -1.0
+        self.desired_pos_z = (goal_request.position_z - self.corr_val_z) #* -1.0
+        self.get_logger().info("corrected positions for robot "+  str(self.desired_pos_x)+str(self.desired_pos_y)+str(self.desired_pos_z))
+        return GoalResponse.ACCEPT
+    
+    def handle_accepted_callback(self, goal_handle):
+        with self.goal_lock:
+            if self.goal_handle is not None and self.goal_handle.is_active:
+                self.get_logger().info('Replacing active goal with new goal.')
+                self.goal_handle.abort()
+            self.goal_handle = goal_handle
+        goal_handle.execute()
+
+    def cancel_callback(self, goal_handle):
+        self.get_logger().info("Goal cancelled. Stopping execution")
+        self.timer.cancel()
+        goal_handle.canceled()
+        return CancelResponse.ACCEPT
            
     def calibration(self):
         #implement calibration for all 3 axis (x,y,z)
         #step all axis to zero position
         #set all position values to zer0
-        self.msg.accel_x = -0.1
-        self.msg.accel_y = 0.05      #!! set direction !!
-        self.msg.accel_z = -0.02     #!! set direction !!
+        self.msg.accel_x = 0.01
+        self.msg.accel_y = 0.0002      #!! set direction !!
+        self.msg.accel_z = 0.0     #!! set direction !!
         self.publish_command() 
 
-        self.calibration_wait_time = 5.0    #set time to wait for calibration
+        self.calibration_wait_time = 10.0    #set time to wait for calibration
         self.calibration_elapsed_time = 0.0
         self.calib_timer = 0.1
         self.calibration_timer = self.create_timer(self.calib_timer, self.calibration_callback)
@@ -67,6 +116,7 @@ class CentralControl(Node):
         if self.calibration_elapsed_time >= self.calibration_wait_time:
             self.calibration_timer.cancel()
 
+            #set values to zero for non simulation runs
             self.msg.accel_x = 0.0
             self.msg.accel_y = 0.0
             self.msg.accel_z = 0.0
@@ -75,8 +125,8 @@ class CentralControl(Node):
             self.set_correction_values()
             print(self.corr_val_x, self.corr_val_y, self.corr_val_z)
             self.get_logger().info("Calibration finished. Robot is in zero position.")
-            self.timer_period = 0.1
-            self.timer = self.create_timer(self.timer_period, self.timer_callback)
+            #self.timer_period = 0.1
+            #self.timer = self.create_timer(self.timer_period, self.timer_callback)
 
     def set_correction_values(self):
         self.corr_val_x = self.pos_x
@@ -96,17 +146,18 @@ class CentralControl(Node):
         self.pos_z = msg.pos_z
 
     #only for testing
+    """
     def cmd_callback(self, msg):        
         self.desired_pos_x = msg.pos_x - self.corr_val_x
         self.desired_pos_y = msg.pos_y - self.corr_val_y
         self.desired_pos_z = msg.pos_z - self.corr_val_z
-
+    """
     def call_pd_controller(self):
         try:  
             
             u_x = self.pd_control_x.berechne(self.desired_pos_x, self.pos_x, self.timer_period)
             u_y = self.pd_control_y.berechne(self.desired_pos_y, self.pos_y, self.timer_period)
-            u_z = self.pd_control_z.berechne(self.desired_pos_y, self.pos_z, self.timer_period)
+            u_z = self.pd_control_z.berechne(self.desired_pos_z, self.pos_z, self.timer_period)
         except AttributeError:
             self.get_logger().warn("Desired position not set. Please set the desired position first.")
             u_x = 0.0
@@ -124,12 +175,13 @@ class CentralControl(Node):
 
 
 def main(args=None):
+    print("Starting Central Control Node")
     rclpy.init(args=args)
 
-    central_control = CentralControl()
-
     try:
-        rclpy.spin(central_control)
+        central_control = CentralControl()
+        mt_executor = MultiThreadedExecutor()
+        rclpy.spin(central_control, executor=mt_executor)
     except KeyboardInterrupt:
         pass
 
