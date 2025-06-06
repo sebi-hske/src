6fdaddfef6786938e288a2caa677e89a50df4d59
sebi-hske <ratzeray@gmail.com>
2025-06-06 10:14:50 +0200

    edit color threshold

--- ro45_portalrobot_controller/ro45_portalrobot_controller/detect_object.py
+++ ro45_portalrobot_controller/ro45_portalrobot_controller/detect_object.py
@@ -46,7 +46,7 @@ class WhiteObjectDetector:
             return
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        _, thresh = cv2.threshold(gray, 200, 240, cv2.THRESH_BINARY)
+        _, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
 
         contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, 
                                      cv2.CHAIN_APPROX_SIMPLE)
